d90eef28e9e70ebb6f5d17e1d3f1cda183063a2a
mkupermann <92990381+mkupermann@users.noreply.github.com>
2025-05-03 17:18:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,36 @@ from setuptools import setup, find_packages
 
 setup(
     name='equiml',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
-        'scikit-learn',  # For machine learning models and utilities
-        'fairlearn',     # For fairness metrics and mitigation
-        'shap',          # For model interpretability
-        'matplotlib',    # For plotting and visualization
-        'seaborn',       # For enhanced visualization
-        'streamlit',     # For interactive web apps
-        'pandas',        # For data manipulation
-        'numpy',         # For numerical operations
-        'scipy',         # For statistical tests (e.g., data drift detection)
-        'joblib',        # For model serialization (saving/loading models)
+        'scikit-learn',
+        'fairlearn',
+        'shap',
+        'matplotlib',
+        'seaborn',
+        'streamlit',
+        'pandas',
+        'numpy',
+        'xgboost',
+        'optuna',
+        'skl2onnx',
+        'onnx',
+        'plotly',
+        'statsmodels',
+        'lime',
+        'scipy',
+        'joblib'
     ],
     author='Michael Kupermann',
-    description='A framework for equitable and responsible machine learning',
+    description='An advanced framework for equitable and responsible machine learning with support for fairness, interpretability, and robustness.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mkupermann/EquiML',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
